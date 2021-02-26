7a23d7499b5c5b7e95efc269096d6c276253cf36
zjquid <39973225+zjquid@users.noreply.github.com>
2021-02-26 03:45:40 -0600

    Update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-feh --bg-scale ~/wiig/s/shneeze.jpg &
+# wallpaper
+
+feh --bg-scale ~/wiig/s/shneeze.jpg
+
+# status bar
+
+while true; do
+	
+	xsetroot -name "$(acpi | awk '{print $4}' | sed s/,//) | $(date '+%Y-%m-%d %H:%M:%S')"
+	sleep 1s
+
+done
